91d8be52782ac7ce465027e9d87a7914d03a05f0
SDNATH-Git <sd.nath1952@gmail.com>
2026-01-15 21:53:57 +0600

    added a pr-7

--- problem7.js
+++ problem7.js
@@ -1,28 +1,28 @@
 // Problem 5: Find Even Numbers in an Array
 
-// function findEvens(arr) {
-//   let evens = [];
-//   for (let num of arr) {
-//     if (num % 2 === 0) {
-//       evens.push(num);
-//     }
-//   }
-//   return evens;
-// }
-
-// // Test
-// console.log(findEvens([1, 2, 3, 4, 5, 6])); 
-
-
-function findEvens(arr){
-  let evens=[];
-  for (let num of arr){
-    if(num % 2 === 0){
+function findEvens(arr) {
+  let evens = [];
+  for (let num of arr) {
+    if (num % 2 === 0) {
       evens.push(num);
     }
   }
   return evens;
 }
 
-//test
-console.log(findEvens([1,2,3,4,5,5,]));
+// Test
+console.log(findEvens([1, 2, 3, 4, 5, 6])); 
+
+
+// function findEvens(arr){
+//   let evens=[];
+//   for (let num of arr){
+//     if(num % 2 === 0){
+//       evens.push(num);
+//     }
+//   }
+//   return evens;
+// }
+
+// //test
+// console.log(findEvens([1,2,3,4,5,5,]));
