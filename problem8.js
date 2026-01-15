f7a50c282aa824fd89b3012755c23ed88a0b03c8
SDNATH-Git <sd.nath1952@gmail.com>
2026-01-15 21:51:04 +0600

    added a pr8

--- problem8.js
+++ problem8.js
@@ -1,14 +1,14 @@
 // Problem 8: Capitalize First Letter of Each words
 
-// function capitalizeWords(str) {
-//   let words = str.split(" ");
-//   let result = [];
-//   for (let word of words) {
-//     let capitalized = word[0].toUpperCase() + word.slice(1);
-//     result.push(capitalized);
-//   }
-//   return result.join(" ");
-// }
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+  for (let word of words) {
+    let capitalized = word[0].toUpperCase() + word.slice(1);
+    result.push(capitalized);
+  }
+  return result.join(" ");
+}
 
 // Test
 console.log(capitalizeWords("hello world")); 
