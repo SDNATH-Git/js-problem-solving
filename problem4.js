d76669278d027fb0ad142cfa9e4f9b9899e155d4
SDNATH-Git <sd.nath1952@gmail.com>
2026-01-28 21:53:21 +0600

    update 28/1/26

--- problem4.js
+++ problem4.js
@@ -1,25 +1,25 @@
-// // Problem 3: Find the Maximum Number
+// // // Problem 3: Find the Maximum Number
 
-// function findMax(arr) {
-//   let max = arr[0];
-//   for (let num of arr) {
-//     if (num > max) {
-//       max = num;
-//     }
-//   }
-//   return max;
+// // function findMax(arr) {
+// //   let max = arr[0];
+// //   for (let num of arr) {
+// //     if (num > max) {
+// //       max = num;
+// //     }
+// //   }
+// //   return max;
+// // }
+
+// // // Test
+// // console.log(findMax([5, 1, 9, 3])); 
+
+
+// // Solution Method 2
+// function findMax(arr){
+//   return Math.max(...arr);
 // }
 
-// // Test
-// console.log(findMax([5, 1, 9, 3])); 
-
-
-// Solution Method 2
-function findMax(arr){
-  return Math.max(...arr);
-}
-
-// text 
-console.log(findMax([3,0,2,8]));
+// // text 
+// console.log(findMax([3,0,2,8]));
 
 
