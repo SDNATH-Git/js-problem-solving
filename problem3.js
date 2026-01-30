708eda1ac728b8e6260cd2c6d983932c0872baef
SDNATH-Git <sd.nath1952@gmail.com>
2026-01-30 19:02:34 +0600

    update - 30/1/26

--- problem3.js
+++ problem3.js
@@ -1,24 +1,24 @@
-// // Problem 3: Check for Palindrome
+// // // Problem 3: Check for Palindrome
 
-// function isPalindrome(str) {
-//   let reversed = "";
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     reversed += str[i];
-//   }
-//   return str === reversed;
+// // function isPalindrome(str) {
+// //   let reversed = "";
+// //   for (let i = str.length - 1; i >= 0; i--) {
+// //     reversed += str[i];
+// //   }
+// //   return str === reversed;
+// // }
+
+// // // Test
+// // console.log(isPalindrome("madam")); 
+// // console.log(isPalindrome("hello")); 
+
+
+// // Sloution Method 2
+// function isPalindrome(str){
+//   return str === str.split('').reverse().join('');
+
 // }
 
-// // Test
-// console.log(isPalindrome("madam")); 
-// console.log(isPalindrome("hello")); 
-
-
-// Sloution Method 2
-function isPalindrome(str){
-  return str === str.split('').reverse().join('');
-
-}
-
-console.log(isPalindrome("madam"));
-console.log(isPalindrome("world"));
+// console.log(isPalindrome("madam"));
+// console.log(isPalindrome("world"));
 
