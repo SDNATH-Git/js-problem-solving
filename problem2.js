a33dda1d852d0b8978463e9261f8fca1e33b4bd9
SDNATH-Git <sd.nath1952@gmail.com>
2026-02-09 22:01:11 +0600

    update 9/2/26

--- problem2.js
+++ problem2.js
@@ -1,15 +1,15 @@
-// // // Problem 2: Count Vowels in a String
+// // Problem 2: Count Vowels in a String
 
-// function CountVowel(str){
-//   let conut = 0 ;
-//   let vowels = 'aeiouAEIOU';
-//   for(let char of str){
-//     if(vowels.includes(char)){
-//       conut++;
-//     }
-//   }
-//   return conut ;
-// }
+function CountVowel(str){
+  let conut = 0 ;
+  let vowels = 'aeiouAEIOU';
+  for(let char of str){
+    if(vowels.includes(char)){
+      conut++;
+    }
+  }
+  return conut ;
+}
 
-// //test
-// console.log(CountVowel('Bkash'));
+//test
+console.log(CountVowel('Bkash'));
