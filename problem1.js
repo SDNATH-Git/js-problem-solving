50ba13cce8b2235fab614483e23ceb364411102e
SDNATH-Git <sd.nath1952@gmail.com>
2026-02-09 22:03:00 +0600

    pr j -01

--- problem1.js
+++ problem1.js
@@ -1,25 +1,25 @@
-// Problem 1: Reverse a String
+// // Problem 1: Reverse a String
 
-// ‍Solution mathod 1   
+// // ‍Solution mathod 1   
 
-// function reverseString(str) {
-//   let reversed = "";
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     reversed += str[i];
-//   }
-//   return reversed;
+// // function reverseString(str) {
+// //   let reversed = "";
+// //   for (let i = str.length - 1; i >= 0; i--) {
+// //     reversed += str[i];
+// //   }
+// //   return reversed;
+// // }
+
+// // // Test
+// // console.log(reverseString("Sakib"));  hsakb
+
+
+// // Solution mathod 2 
+// function reverseString(str){
+//   return str.split('').reverse('').join('');
+
 // }
-
-// // Test
-// console.log(reverseString("Sakib"));  hsakb
-
-
-// Solution mathod 2 
-function reverseString(str){
-  return str.split('').reverse('').join('');
-
-}
-console.log(reverseString("Bkash"));
+// console.log(reverseString("Bkash"));
 
 
 
