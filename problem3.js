0d6a9735c02cb78e5b8a4cf94b0978f691d6b6e9
SDNATH-Git <sd.nath1952@gmail.com>
2026-02-19 21:43:46 +0600

    pr---3

--- problem3.js
+++ problem3.js
@@ -1,16 +1,16 @@
 // Problem 3: Check for Palindrome
 
-function isPalindrome(str) {
-  let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return str === reversed;
-}
+// function isPalindrome(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return str === reversed;
+// }
 
-// Test
-console.log(isPalindrome("madam")); 
-console.log(isPalindrome("hello")); 
+// // Test
+// console.log(isPalindrome("madam")); 
+// console.log(isPalindrome("hello")); 
 
 
 // Sloution Method 2
