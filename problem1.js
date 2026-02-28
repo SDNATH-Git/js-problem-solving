84ff6df0fd88cefd79f129d220ef606d81095b85
SDNATH-Git <sd.nath1952@gmail.com>
2026-02-28 21:55:13 +0600

    update 28/2/26

--- problem1.js
+++ problem1.js
@@ -2,13 +2,13 @@
 
 // ‍Solution mathod 1   
 
-function reverseString(str) {
-  let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
-}
+// function reverseString(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
 
 // Test
 console.log(reverseString("Sakib"));  hsakb
