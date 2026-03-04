ebe5361ca376a632393d9720c4d948851fec086e
SDNATH-Git <sd.nath1952@gmail.com>
2026-03-04 23:00:17 +0600

    update 4/3/26

--- problem7.js
+++ problem7.js
@@ -11,18 +11,18 @@ function findEvens(arr) {
 }
 
 // Test
-console.log(findEvens([1, 2, 3, 4, 5, 6])); 
+// console.log(findEvens([1, 2, 3, 4, 5, 6])); 
 
 
-function findEvens(arr){
-  let evens=[];
-  for (let num of arr){
-    if(num % 2 === 0){
-      evens.push(num);
-    }
-  }
-  return evens;
-}
+// function findEvens(arr){
+//   let evens=[];
+//   for (let num of arr){
+//     if(num % 2 === 0){
+//       evens.push(num);
+//     }
+//   }
+//   return evens;
+// }
 
 //test Find Even Numbers in an Array
 
